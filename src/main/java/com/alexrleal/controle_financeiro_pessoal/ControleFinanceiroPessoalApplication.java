0f02dd1cecf14d6a4b97d88ae02e3e4635b72526
Alexandre Leal <alexrleal0@gmail.com>
2024-10-29 09:56:13 -0300

    Created project

---
+++ src/main/java/com/alexrleal/controle_financeiro_pessoal/ControleFinanceiroPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.alexrleal.controle_financeiro_pessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFinanceiroPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFinanceiroPessoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexrleal/controle_financeiro_pessoal/ControleFinanceiroPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexrleal.controle_financeiro_pessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
